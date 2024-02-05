01740edffc24736b7b169e96da64ece2fc591ff3
TechSmith90210 <sshaikhdev@gmail.com>
2024-02-06 00:22:41 +0530

    init song Player 0.2
    some ui changes left mainly the chips

--- lib/Presentation/Screens/song_player_screen.dart
+++ lib/Presentation/Screens/song_player_screen.dart
@@ -1,6 +1,9 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:grapewine_music_app/Presentation/widgets/ArtistChipsWidget.dart';
+import 'package:interactive_slider/interactive_slider.dart';
 
 import '../../Colors/colors.dart';
 
@@ -14,96 +17,170 @@ class SongPlayerScreen extends StatefulWidget {
 class _SongPlayerScreenState extends State<SongPlayerScreen> {
   @override
   Widget build(BuildContext context) {
+    double _value = 2.0;
     return Scaffold(
-        backgroundColor: darkblueBubbleColor,
-        body: Container(
-          margin: EdgeInsets.only(left: 16, right: 16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                // crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 70,
-                  ),
-                  Text(
-                    'Now Playing',
-                    style: GoogleFonts.redHatDisplay(
-                        color: whiteColor,
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500),
-                  ),
-                  Text(
-                    'At The Party',
-                    style: GoogleFonts.redHatDisplay(
-                        color: yellowColor,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  Icon(
-                    Icons.keyboard_arrow_down_sharp,
-                    size: 60,
-                    color: darkgreyColor,
-                  ),
-                  Container(
-                    height: 300,
-                    width: 300,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://www.universalmusic.ca/wp-content/uploads/2023/11/AtTheParty-Single-Artwork_FINAL-scaled.jpg'),
-                          fit: BoxFit.cover,
+      backgroundColor: darkblueBubbleColor,
+      body: Container(
+        // margin: EdgeInsets.only(left: 16, right: 16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  'Now Playing',
+                  style: GoogleFonts.redHatDisplay(
+                      color: whiteColor,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  'At The Party',
+                  style: GoogleFonts.redHatDisplay(
+                      color: yellowColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700),
+                ),
+                Icon(
+                  Icons.keyboard_arrow_down_sharp,
+                  size: 60,
+                  color: darkgreyColor,
+                ),
+                Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            'https://www.universalmusic.ca/wp-content/uploads/2023/11/AtTheParty-Single-Artwork_FINAL-scaled.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                      borderRadius: BorderRadius.circular(11)),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.textsms_outlined,
+                      color: whiteColor,
+                      size: 30,
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'At The Party',
+                          style: GoogleFonts.redHatDisplay(
+                              color: whiteColor,
+                              fontSize: 28,
+                              fontWeight: FontWeight.w800),
                         ),
-                        borderRadius: BorderRadius.circular(11)),
+                        Text(
+                          'Kid Cudi, Pharell Williams, Travis Scott',
+                          style: GoogleFonts.redHatDisplay(
+                              color: darkgreyColor,
+                              fontSize: 13,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 25,
+                    ),
+                    Icon(
+                      Icons.favorite_border_rounded,
+                      color: whiteColor,
+                      size: 30,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  width: 330,
+                  child: ProgressBar(
+                    progress: Duration(seconds: 30),
+                    total: Duration(minutes: 3, seconds: 58),
+                    buffered: Duration(minutes: 1, seconds: 30),
+                    progressBarColor: whiteColor,
+                    thumbColor: whiteColor,
+                    baseBarColor: backgroundColor,
+                    bufferedBarColor: greyColor,
+                    thumbGlowColor: purpleColor,
+                    thumbGlowRadius: 0,
+                    timeLabelPadding: 5,
+                    timeLabelTextStyle: GoogleFonts.redHatDisplay(
+                      color: greyColor,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                  SizedBox(
-                    height: 20,
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.fast_rewind_rounded,
+                      color: whiteColor,
+                      size: 60,
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Icon(
+                      Icons.play_arrow_rounded,
+                      color: whiteColor,
+                      size: 90,
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Icon(
+                      Icons.fast_forward_rounded,
+                      color: whiteColor,
+                      size: 60,
+                    ),
+                  ],
+                ),
+                Container(
+                  width: 330,
+                  child: InteractiveSlider(
+                    startIcon: const Icon(Icons.volume_down),
+                    endIcon: const Icon(Icons.volume_up),
+                    brightness: Brightness.light,
+                    initialProgress: 0.0,
+                    iconSize: 30,
+                    unfocusedHeight: 20,
+                    min: 1.0,
+                    max: 15.0,
+                    onChanged: (value) => setState(() => _value = value),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.textsms_outlined,
-                        color: whiteColor,
-                        size: 30,
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Column(
-                        children: [
-                          Text(
-                            'At The Party',
-                            style: GoogleFonts.redHatDisplay(
-                                color: whiteColor,
-                                fontSize: 28,
-                                fontWeight: FontWeight.w800),
-                          ),
-                          Text(
-                            'Kid Cudi, Pharell Williams, Travis Scott',
-                            style: GoogleFonts.redHatDisplay(
-                                color: darkgreyColor,
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 25,
-                      ),
-                      Icon(
-                        Icons.favorite_border_rounded,
-                        color: whiteColor,
-                        size: 30,
-                      )
-                    ],
+                ),
+                Container(
+                  height: 40,
+                  width: 370,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 4,
+                    itemBuilder: (context, index) {
+                      return ArtistChipsWidget();
+                    },
                   ),
-                ],
-              ),
-            ],
-          ),
-        ));
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/Presentation/widgets/ArtistChipsWidget.dart
+++ lib/Presentation/widgets/ArtistChipsWidget.dart
@@ -13,21 +13,20 @@ class ArtistChipsWidget extends StatefulWidget {
 class _ArtistChipsWidgetState extends State<ArtistChipsWidget> {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Chip(
-            shape: StadiumBorder(),
-            avatar: CircleAvatar(
-              foregroundImage: NetworkImage(
-                  'https://i.scdn.co/image/ab6761610000e5eb876faa285687786c3d314ae0'),
-            ),
-            backgroundColor: eerieblackColor,
-            label: Text(
-              'Kid Cudi',
-              style: GoogleFonts.redHatDisplay(
-                  color: whiteColor, fontWeight: FontWeight.w800),
-            )),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 10),
+      child: Chip(
+          shape: StadiumBorder(),
+          avatar: CircleAvatar(
+            foregroundImage: NetworkImage(
+                'https://i.scdn.co/image/ab6761610000e5eb876faa285687786c3d314ae0'),
+          ),
+          backgroundColor: eerieblackColor,
+          label: Text(
+            'Kid Cudi',
+            style: GoogleFonts.redHatDisplay(
+                color: whiteColor, fontWeight: FontWeight.w800),
+          )),
     );
   }
 }
